4e3b3bbbe216182756da66bc9093638e8ca4a41a
Emil <vagabov_emil@mail.ru>
2023-04-11 23:59:47 +0300

    added a module of a high lÑevel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,25 +7,29 @@ import {AuthModule} from "./auth/auth.module";
 import {StoreModule} from "@ngrx/store";
 import {StoreDevtoolsModule} from "@ngrx/store-devtools";
 import {EffectsModule} from "@ngrx/effects";
+import {TopBarModule} from "./shared/modules/topBar/topBar.module";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    EffectsModule.forRoot([]),
-    AuthModule,
-    StoreModule.forRoot({}),
-    StoreDevtoolsModule.instrument({
-      maxAge: 25,
-      logOnly: !isDevMode(),
-      autoPause: true,
-      trace: false,
-      traceLimit: 75,
-    }),
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        EffectsModule.forRoot([]),
+        AuthModule,
+        StoreModule.forRoot({}),
+        StoreDevtoolsModule.instrument({
+            maxAge: 25,
+            logOnly: !isDevMode(),
+            autoPause: true,
+            trace: false,
+            traceLimit: 75,
+        }),
+       TopBarModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
